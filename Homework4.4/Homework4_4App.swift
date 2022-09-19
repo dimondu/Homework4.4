824e8fbd81a33cceba1ec11bcd99201feab9c30c
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-09-19 16:07:46 +0300

    Initial Commit

---
+++ Homework4.4/Homework4_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework4_4App.swift
+//  Homework4.4
+//
+//  Created by Дмитрий Дуров on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework4_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
