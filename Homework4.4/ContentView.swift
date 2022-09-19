349da8b37f15f765dbb3b1947a333936f81f215b
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-09-19 20:42:58 +0300

    Final version

--- Homework4.4/ContentView.swift
+++ Homework4.4/ContentView.swift
@@ -10,43 +10,40 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var targetValue = Int.random(in: 0...100)
-    @State private var currentValue: Float = 50.0
+    @State private var currentValue = 3.0
+    @State private var showAlert = false
+    
     var body: some View {
         VStack {
-            
             Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
-            
             HStack {
                 Text("0")
-                
-                SliderUIKIT(value: $currentValue)
-                    
-            
+                SliderUIKIT(value: $currentValue, opacity: CGFloat(computeScore()))
                 Text("100")
-                
-            
             }
             .padding()
-            Button("Проверь меня!") {
-            print("hello")
-            }
-            .padding(.bottom, 20)
             
+            Button("Проверь меня!") {
+                self.showAlert = true
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your score"),
+                      message: Text("\(computeScore())"),
+                      dismissButton: .default(Text("Ok")))
+            }
+            .padding()
             
             Button("Начать заново") {
                 targetValue = Int.random(in: 0...100)
             }
         }
-        .padding()
         
     }
-//    private func computeScore() -> Int {
-//        let difference = abs(targetValue - lround(Double(currentValue)))
-//        return 100 - difference
-//    }
-
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
+    }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Homework4.4/SliderUIKit.swift
+++ Homework4.4/SliderUIKit.swift
@@ -9,51 +9,50 @@ import SwiftUI
 
 struct SliderUIKIT: UIViewRepresentable {
     
-    @Binding var value: Float
+    @Binding var value: Double
+    let opacity: CGFloat
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
-        slider.thumbTintColor = UIColor(displayP3Red: 1, green: 0, blue: 0, alpha: 0.3)
+
         slider.minimumValue = 0
         slider.maximumValue = 100
         
         slider.addTarget(
             context.coordinator,
             action: #selector(Coordinator.sliderReleased),
-            for: .editingDidEnd
+            for: .valueChanged
         )
         
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
-        uiView.value = value
+        uiView.value = Float(value)
+        uiView.thumbTintColor = UIColor(displayP3Red: 1, green: 0, blue: 0, alpha: opacity / 100)
     }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(value: $value)
     }
-    
-    
 }
-
 
 extension SliderUIKIT {
     class Coordinator: NSObject {
-        @Binding var value: Float
+        @Binding var value: Double
         
-        init(value: Binding<Float>) {
+        init(value: Binding<Double>) {
             self._value = value
         }
         
         @objc func sliderReleased(_ sender: UISlider) {
-            value = sender.value
+            value = Double(sender.value)
         }
     }
 }
 
 struct SliderUIKit_Previews: PreviewProvider {
     static var previews: some View {
-        SliderUIKIT(value: .constant(50.0))
+        SliderUIKIT(value: .constant(50.0), opacity: 0.2)
     }
 }
